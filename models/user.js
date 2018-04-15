2e5dd50ef0f5fdfdab3e177f6a86d8aa4885de3a
David Dinh <dmdinh22@gmail.com>
2018-04-15 00:39:39 -0700

    added user registration

--- models/user.js
+++ models/user.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 const bcrypt = require('bcryptjs');
-const config = require('../models/database');
+const config = require('../config/database');
 
 // user schema
 const UserSchema = mongoose.Schema({
@@ -22,3 +22,24 @@ const UserSchema = mongoose.Schema({
 });
 
 const User = module.exports = mongoose.model('User', UserSchema);
+
+module.exports.getUserById = function(id, callback) {
+    User.findById(id, callback);
+};
+
+module.exports.getUserByUsername = function(username, callback) {
+    const query = { username: username };
+    User.findOne(query, callback);
+};
+
+module.exports.addUser = function(newUser, callback) {
+    bcrypt.genSalt(10, (err, salt) => {
+        bcrypt.hash(newUser.password, salt, (err, hash) => {
+            if (err) {
+                throw err;
+            }
+            newUser.password = hash;
+            newUser.save(callback);
+        });
+    });
+};
