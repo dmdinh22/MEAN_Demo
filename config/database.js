b6ec2f8067bb231d9ee3802ab60ae4af52dbf5f9
David Dinh <dmdinh22@gmail.com>
2018-04-17 23:37:52 -0700

    linked mongodb

--- config/database.js
+++ config/database.js
@@ -1,4 +1,4 @@
 module.exports = {
-    database: 'mongodb://localhost:27017/meandemo',
+    database: 'mongodb://dmdinh:ironmansucks@ds149309.mlab.com:49309/meanauthdemo',
     secret: 'secretagentman'
-}
+};
