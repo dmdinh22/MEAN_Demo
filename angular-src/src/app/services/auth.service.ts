f90bb2c8ccc9e1618e97cab50492cb81cd2c3023
David Dinh <dmdinh22@gmail.com>
2018-04-17 20:59:22 -0700

    added basic client side profile

--- angular-src/src/app/services/auth.service.ts
+++ angular-src/src/app/services/auth.service.ts
@@ -14,14 +14,23 @@ export class AuthService {
     headers.append('Content-Type', 'application/json');
     return this.http.post('http://localhost:3000/users/register', user, { headers: headers})
                     .map(res => res.json());
-  }
+  } //registerUser
 
   authenticateUser(user) {
     let headers = new Headers();
     headers.append('Content-Type', 'application/json');
     return this.http.post('http://localhost:3000/users/authenticate', user, { headers: headers})
                     .map(res => res.json());
-  }
+  } //authenticateUser
+
+  getProfile() {
+    let headers = new Headers();
+    this.loadToken(); // grab token for aut header
+    headers.append('Authorization', this.authToken);
+    headers.append('Content-Type', 'application/json');
+    return this.http.get('http://localhost:3000/users/profile', { headers: headers})
+                    .map(res => res.json());  
+  } //getProfile
 
   storeUserData(token, user) {
     localStorage.setItem('id_token', token);
@@ -30,6 +39,11 @@ export class AuthService {
     this.authToken = token;
     this.user = user;
   } //storeUserData
+
+  loadToken() {
+    const token = localStorage.getItem('id_token');
+    this.authToken = token;
+  } //loadToken
 
   logout() {
     this.authToken = null;
